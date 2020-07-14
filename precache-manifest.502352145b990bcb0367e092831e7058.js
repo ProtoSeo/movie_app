d9a8a280b41182084be4714eb75266f986f2e69a
SeoSeunghun <201602012@o.cnu.ac.kr>
2020-07-14 16:02:20 +0900

    Updates

---
+++ precache-manifest.502352145b990bcb0367e092831e7058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0a5f80de181ae7ee5ac8b53b9603e7",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "408bfed6d283d0342be5",
+    "url": "/movie_app/static/css/main.22d9ae80.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "408bfed6d283d0342be5",
+    "url": "/movie_app/static/js/main.e6fdf714.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
